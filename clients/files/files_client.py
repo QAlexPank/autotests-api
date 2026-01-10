2fe80c033308a221c44e36866ccf9fb695fbbd7c
Alexey Pankratov <pae.86@mail.ru>
2026-01-10 20:26:43 +0300

    Улучшение работы с API клиентами с помощью Enum

--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -6,6 +6,7 @@ from httpx import Response
 from clients.api_client import APIClient
 from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema
 from clients.private_http_builder import get_private_http_client, AuthenticationUserSchema
+from tools.routes import APIRoutes
 
 
 class FilesClient(APIClient):
@@ -22,7 +23,7 @@ class FilesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post(
-            "/api/v1/files",
+            APIRoutes.FILES,
             data=request.model_dump(by_alias=True, exclude={'upload_file'}),
             files={"upload_file": request.upload_file.read_bytes()}
         )
@@ -35,7 +36,7 @@ class FilesClient(APIClient):
         :param file_id: Идентификатор файла.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(f"/api/v1/files/{file_id}")
+        return self.get(f"{APIRoutes.FILES}/{file_id}")
 
     @allure.step("Delete file by id {file_id}")
     def delete_file_api(self, file_id: str) -> Response:
@@ -45,7 +46,7 @@ class FilesClient(APIClient):
         :param file_id: Идентификатор файла.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.delete(f"/api/v1/files/{file_id}")
+        return self.delete(f"{APIRoutes.FILES}/{file_id}")
 
     def create_file(self, request: CreateFileRequestSchema) -> CreateFileResponseSchema:
         """
